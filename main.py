721cf16971e187bba2ff1a1880a71a69c10e0fc7
mesmacosta <mesmacosta@gmail.com>
2019-10-28 16:20:34 -0300

    FIX main file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
-from datacatalog_fileset_enricher import DatacatalogFilesetEnricherCLI
+from datacatalog_fileset_enricher import datacatalog_fileset_enricher_cli
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    DatacatalogFilesetEnricherCLI.run()
+    datacatalog_fileset_enricher_cli.DatacatalogFilesetEnricherCLI.run()
